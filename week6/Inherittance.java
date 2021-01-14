faada8940fd7f4f4306326c52cdd5e81858c9c9b
EmeraldEye1 <77324402+EmeraldEye1@users.noreply.github.com>
2021-01-14 20:51:37 +0700

    Update Inherittance.java

--- week6/Inherittance.java
+++ week6/Inherittance.java
@@ -7,7 +7,7 @@ public class Inherittance {
         career2 car  =new career2("Eva",19,"female");
         career1 care  =new career1("Lucus",29,"Male");
         career3 caree =new career3("Jordi",27,"Male");
-        career4 career =new career4("Dasha",25,"Male");
+        career4 career =new career4("Dasha",25,"female");
         
         
         art.genre ="hip hob  ";
