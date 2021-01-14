02e0e6d48183341d38bfe7a0f8d74d3be2ab736b
EmeraldEye1 <Tan_sanook02@hotmail.com>
2021-01-14 20:42:18 +0700

    week6-5

---
+++ week6/career1.java
@@ -0,0 +1,17 @@
+package week6;
+
+public class career1 extends Person {
+    
+    String career;
+   
+   public career1(String name ,int age,String gender){
+       this.age = age;
+       this.name = name;
+       this.gender = gender;
+   }
+       public void job(){
+           System.out.println(name +" job is "+career);
+   
+}
+}
+
