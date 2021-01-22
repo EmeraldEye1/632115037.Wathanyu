2ed004101bcb8fd23761ac9b6e8278cf8f55aaa5
EmeraldEye1 <Tan_sanook02@hotmail.com>
2021-01-22 21:26:33 +0700

    https://github.com/EmeraldEye1/632115037.Wathanyu.git

---
+++ week8/mountainbike.java
@@ -0,0 +1,21 @@
+package week8;
+    public class mountainbike extends biker{
+        public mountainbike(int Speed){
+            Speeder(Speed);
+            System.out.println("MountainBike");
+            if(checkingSpeed(Speed1())){
+                Speeder(Speed1()-Speed);
+            }
+        }
+        public void Speedup(int gearR){
+            Speeder(Speed1()+(gearR* Speed2()));
+            if (checkingSpeed(Speed1())) {
+                Speeder(Speed1() - (gearR * Speed2()));
+            }
+        }
+    
+        public void BreakBike() {
+            Speeder(Speed1() - Speed2());
+        }
+    }
+
